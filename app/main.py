af19cb7be174fe437b462b078c4f1f9cc6ad3a17
The-CoDexR3kt <krayed96@gmail.com>
2025-09-12 16:29:12 +0530

    experiment

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ from app.api.endpoints import auth, doctor, patient, translate
 
 Base.metadata.create_all(bind=engine)
 
+app = FastAPI()
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Change this in production
@@ -12,8 +15,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(doctor.router, prefix="/doctor", tags=["doctor"])
